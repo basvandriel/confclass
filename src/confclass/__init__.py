203fa4532e40f6c2db488d77eb586cfcf84d6829
Bas van Driel <contact@basvandriel.nl>
2023-10-26 20:32:52 +0200

    yes

--- src/confclass/__init__.py
+++ src/confclass/__init__.py
@@ -1,18 +1,12 @@
-# def confclass(cls: object | None = None):
-#     # When confclass is called without "()" (@confclass), an error should appear
-#     # In that case, cls is filled in. Also, funcs aren't allowed
-#     if cls is None:
-#         raise ValueError("Decorator can only be used with parameters")
-#     if not isclass(cls):
-#         raise ValueError("Decorator can only be used on a class")
-
-#     def wrap(cls):
-#         return ClassProcessor().process(cls)
-
-#     return wrap
+from inspect import isclass
 
 
 def confclass[T: object](cls: T) -> T:
+    if cls is None:
+        raise ValueError("Decorator can only be used with parameters")
+    if not isclass(cls):
+        raise ValueError("Decorator can only be used on a class")
+
     def wrap(cls: T): 
         setattr(cls, '__IS_CONCLASS__', True) 
         return cls
