c23a10117955dec35802ac6342580bafb7aaa9dd
Bas van Driel <contact@basvandriel.nl>
2023-11-10 14:41:06 +0100

    cleanup

--- tests/test_jsonparse.py
+++ tests/test_jsonparse.py
@@ -9,5 +9,3 @@ class TestingPayload:
 def test_jsonparse(testdir: Path):
     filepath = testdir / 'test.json'
     result: TestingPayload = JSONWriter().read_into(filepath, TestingPayload)
-
-    assert 1 == 1
