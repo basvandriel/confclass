13b8b5eaa1c6cc5221b37c8107d89478a9accb30
Bas van Driel <contact@basvandriel.nl>
2023-11-15 21:24:03 +0100

    cleanup

--- src/configurationclass/filler/env_filler.py
+++ src/configurationclass/filler/env_filler.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
 from typing import Any, Type, TypeVar, override
-from configurationclass.filler.flat_object_filler import ObjectFiller
+from configurationclass.filler.flat_object_filler import FlatObjectfiller
 from configurationclass.row import Row
 
 T = TypeVar('T', bound=object)
 
-# TODO don't inherit from nested structure fillers.
-class EnvObjectFiller(ObjectFiller[T]):
+class EnvObjectFiller(FlatObjectfiller[T]):
     # There are configuration formats which always 
     # read as a string. Lets cast :D
     __SUPPORTED_CASTS: list[Type[object]] = [
@@ -25,6 +24,7 @@ class EnvObjectFiller(ObjectFiller[T]):
         obj = cls()
     
         for k,v in data.items():
+            if not self._should_set_attr(cls, k): continue
             row = Row[T](cls, k, v)
             
             expected_type: Type[Any] = row.valuetype
@@ -35,9 +35,7 @@ class EnvObjectFiller(ObjectFiller[T]):
                 caster:  Type[float | int | str] = expected_type
                 row.value = caster(v)
 
-            if not self._should_set_attr(cls, k):
-                continue
                         
-            setattr(obj, k, self._resolve_value(row))
+            setattr(obj, k, row.value)
             
         return obj
--- src/configurationclass/filler/flat_object_filler.py
+++ src/configurationclass/filler/flat_object_filler.py
@@ -6,7 +6,8 @@ from configurationclass.row import Row
 
 T = TypeVar('T', bound=object)
 
-class ObjectFiller(Generic[T]):
+
+class FlatObjectfiller(Generic[T]):
     _class: Type[T]
     _overwrite_defaults: bool
     
@@ -14,9 +15,6 @@ class ObjectFiller(Generic[T]):
         self._class = type   
         self._overwrite_defaults = overwrite_defaults
         
-    def _should_set_attr(self, cls: type, k: str) -> bool:
-        return self._overwrite_defaults or not hasattr(cls, k)
-     
     def _validate_input_attributes(
         self, data: dict[str, Any], matching_class: Type[Any]
     ) -> None:
@@ -26,9 +24,32 @@ class ObjectFiller(Generic[T]):
         diff = [x for x in expected if x not in set(data.keys())]
 
         if diff:
-            msg = f"Missing input attributes for {classname}: {', '.join(diff)}"
-            raise AttributeError(msg)
+            raise AttributeError(
+                f"Missing input attributes for {classname}: {', '.join(diff)}"
+            )
+        
+    def _should_set_attr(self, cls: type, k: str) -> bool:
+        return self._overwrite_defaults or not hasattr(cls, k)
 
+    def _resolve_obj(self, cls: Type[T], data: dict[str, Any]) -> T:
+        obj = cls()
+        
+        for k,v in data.items():
+            if type(v) == dict or not self._should_set_attr(cls, k): continue 
+            row = Row[T](cls, k, v)
+                        
+            setattr(obj, k, row.value)
+            
+        return obj
+
+    def fill(self, data: dict[str, Any]) -> T:
+        self._validate_input_attributes(
+            data, self._class
+        )
+        return self._resolve_obj(self._class, data)
+    ...
+
+class ObjectFiller(FlatObjectfiller[T]):
     def _validate_class_annotations(self, row: Row[T]) -> None:        
         if row.key not in row.type.__annotations__:
             raise AttributeError(f"Attribute '{row.key}' not found in class")
@@ -62,12 +83,7 @@ class ObjectFiller(Generic[T]):
             if not self._should_set_attr(cls, k):
                 continue
                         
+            
             setattr(obj, k, self._resolve_value(row))
             
         return obj
-
-    def fill(self, data: dict[str, Any]) -> T:
-        self._validate_input_attributes(
-            data, self._class
-        )
-        return self._resolve_obj(self._class, data)
