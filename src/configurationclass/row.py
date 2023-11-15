200d0366767a44a727b65e922f6f5bb53178c9da
Bas van Driel <contact@basvandriel.nl>
2023-11-15 19:34:38 +0100

    add keytype

--- src/configurationclass/row.py
+++ src/configurationclass/row.py
@@ -1,3 +1,5 @@
+
+
 from dataclasses import dataclass
 from typing import Any, Generic, Type, TypeVar
 
@@ -8,3 +10,8 @@ class Row(Generic[T]):
     type: Type[T]
     key: str
     value: Any
+    
+    @property
+    def keytype(self) -> Type[object]:
+        attrs = self.type.__annotations__
+        return attrs[self.key] # type: ignore[no-any-return]
